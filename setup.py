3147b8e338f7a4e85ded5e38841b9775a50ec9a7
Guenter Bartsch <guenter@zamia.org>
2018-07-23 23:47:24 +0200

    fix dependencies, cmd name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name                 = 'kaldi-adapt-lm',
-    version              = '0.1.0',
+    version              = '0.1.1',
     description          = 'Adapt Kaldi-ASR nnet3 chain models from Zamia-Speech.org to a different language model.',
     long_description     = open('README.md').read(),
     author               = 'Guenter Bartsch',
@@ -10,8 +10,8 @@ setup(
     maintainer           = 'Guenter Bartsch',
     maintainer_email     = 'guenter@zamia.org',
     url                  = 'https://github.com/gooofy/kaldi-adapt-lm',
-    packages             = ['kaldiadaptlm'],
-    install_requires     = [ ],
+    packages             = [ 'kaldiadaptlm' ],
+    install_requires     = [ 'py-nltools' ],
     scripts              = [ 'kaldi-adapt-lm' ],
     classifiers          = [
                                'Operating System :: POSIX :: Linux',
